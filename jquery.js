8c59abfcdd2da73a9040654ef90708ebc679dd88
费冒斯 <644456285@qq.com>
2016-07-05 22:38:32 +0800

    myblog

--- jquery.js
+++ jquery.js
@@ -19,12 +19,23 @@ $('#nav>li').eq(0)
 $('#skill #left>li').each(function(index) {
 	$(this).css({'color': arrColor[index],'fontWeight':'bold'});
 });
-$('#nav>li').mouseenter(function(event) {
-	$(this)
-		.css('transition','0.35s')
-		.css({'fontSize': '22px','textShadow':'2px 2px 4px black'});
-	$(this).siblings().css({'fontSize': '18px','textShadow':'none'});
+
+$('#wrap>div').eq(0).css('display', 'block');
+$('#nav>li').each(function(index, el) {
+	$(this).mouseenter(function(event) {
+		$(this)
+			.css('transition','0.35s')
+			.css({'fontSize': '22px','textShadow':'2px 2px 4px black'});
+		$(this).siblings().css({'fontSize': '18px','textShadow':'none'});
+		$('#wrap>div').stop(true, true);
+		var n=0;
+		$('#wrap>div').eq(index).siblings('div').fadeOut(1000,function(){
+			n++;
+			if(n==4){$('#wrap>div').eq(index).fadeIn(1000);}
+		});
+	});
 });
+
 
 $('#skill #right div').each(function(index) {
 	$(this).animate({'width': arrWidth[index]},fnRandom(),function(){repeat(index)});
@@ -61,9 +72,24 @@ $('#project #left').click(function(event) {
 	bul=false;
 	if(n!=0){move();}
 });
-
+// $('#work ul').css('box-shadow','-4px -2px 15px 0px black');
+// 
 // $('#basic').slideDown(1000);
 // $('#skill').slideDown(1000);
-$('#project').slideDown(1000);
+// $('#project').slideDown(1000);
+// $('#work').slideDown(1000);
+$('#work ul').eq(0).css('display', 'block');
+$('#work div')
+.eq(0).css('background', '#ffefef').end()
+.eq(1).css('background', '#c5def2').end()
+.eq(2).css('background', '#b2e4b2').end()
+.eq(3).css('background', '#fcecd0').end()
+$('#work div').each(function(index, el) {
+	$(this).click(function(event) {
+		$('#work ul').eq(index).slideToggle(1000);
+		$('#work ul').eq(index).siblings('ul').slideUp(1000);
+	});
+});
+
 
 
